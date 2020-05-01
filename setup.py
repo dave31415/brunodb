0991a06e54164520f9f376b774eb333a4580bef4
David Johnston <dajohnst@thoughtworks.com>
2020-04-30 22:10:59 -0400

    version bump 0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='brunodb',
-    version='0.1.1',
+    version='0.1.2',
     description='Useful wrapper for SQLite',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
